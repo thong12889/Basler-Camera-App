d061853d51e1a2a82908aa9f4d68563d1dee16a0
thong12889 <thongchai_12889@hotmail.com>
2021-01-13 08:16:15 +0900

    first commit

---
+++ BaslerCam1/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿
+namespace BaslerCam1
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnOneShot = new System.Windows.Forms.Button();
+            this.btnContinuousShot = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnOneShot
+            // 
+            this.btnOneShot.Location = new System.Drawing.Point(59, 108);
+            this.btnOneShot.Name = "btnOneShot";
+            this.btnOneShot.Size = new System.Drawing.Size(75, 23);
+            this.btnOneShot.TabIndex = 1;
+            this.btnOneShot.Text = "OneShot";
+            this.btnOneShot.UseVisualStyleBackColor = true;
+            this.btnOneShot.Click += new System.EventHandler(this.btnOneShot_Click);
+            // 
+            // btnContinuousShot
+            // 
+            this.btnContinuousShot.Location = new System.Drawing.Point(59, 165);
+            this.btnContinuousShot.Name = "btnContinuousShot";
+            this.btnContinuousShot.Size = new System.Drawing.Size(75, 23);
+            this.btnContinuousShot.TabIndex = 2;
+            this.btnContinuousShot.Text = "Continuous";
+            this.btnContinuousShot.UseVisualStyleBackColor = true;
+            this.btnContinuousShot.Click += new System.EventHandler(this.btnContinuousShot_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(59, 212);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 3;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(236, 25);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(410, 391);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.LargeChange = 500;
+            this.trackBar1.Location = new System.Drawing.Point(12, 41);
+            this.trackBar1.Maximum = 100000;
+            this.trackBar1.Minimum = 1000;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(182, 45);
+            this.trackBar1.TabIndex = 5;
+            this.trackBar1.Value = 1000;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(56, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 16);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Exposure Times";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(99, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(11, 12);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "0";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(59, 371);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Save";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(25, 346);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(153, 19);
+            this.textBox1.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(57, 331);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(96, 12);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Photo Name (.jpg)";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(654, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnContinuousShot);
+            this.Controls.Add(this.btnOneShot);
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.MainForm_FormClosed);
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnOneShot;
+        private System.Windows.Forms.Button btnContinuousShot;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ BaslerCam1/Form1.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Basler.Pylon;
+using System.Diagnostics;
+using System.IO;
+
+namespace BaslerCam1
+{
+    public partial class MainForm : Form
+    {
+        public int CameraNumber = CameraFinder.Enumerate().Count();
+        public delegate void CameraImage(Bitmap bmp);
+        public event CameraImage CameraImageEvent;
+        Camera camera;
+        bool GrabOver = false;
+        PixelDataConverter pxConvert = new PixelDataConverter();
+        
+        public MainForm()
+        {
+            InitializeComponent();
+            CameraImageEvent += MainForm_CameraImageEvent;
+
+        }
+
+        private void MainForm_CameraImageEvent(Bitmap bmp)
+        {
+            pictureBox1.Invoke(new MethodInvoker(delegate
+            {
+                Bitmap old = pictureBox1.Image as Bitmap;
+                pictureBox1.Image = bmp;
+                if (old != null)
+                {
+                    old.Dispose();
+                }
+            }));
+        }
+        void Unanble()
+        {
+            btnStop.Enabled = false;
+            btnOneShot.Enabled = false;
+            btnContinuousShot.Enabled = false;
+
+        }
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            if (CameraNumber > 0)
+            {
+                CameraInit();               
+                
+            }
+            else
+            {
+                MessageBox.Show("Not connect to camera");
+                Unanble();
+            }
+        }
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            label2.Text = trackBar1.Value.ToString();
+            camera.Parameters[PLCamera.ExposureTime].SetValue(trackBar1.Value);
+        }
+        private void btnOneShot_Click(object sender, EventArgs e)
+        {
+            OneShot();
+        }
+
+        private void btnContinuousShot_Click(object sender, EventArgs e)
+        {
+            KeepShot();
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            Stop();
+        }
+
+        public void CameraInit()
+        {
+            camera = new Camera();
+            camera.CameraOpened += Configuration.AcquireContinuous;
+            camera.ConnectionLost += Camera_ConnectionLost;
+            camera.StreamGrabber.GrabStarted += StreamGrabber_GrabStarted;
+            camera.StreamGrabber.ImageGrabbed += StreamGrabber_ImageGrabbed;
+            camera.StreamGrabber.GrabStopped += StreamGrabber_GrabStopped;
+
+            camera.Open();
+        }
+        private void StreamGrabber_GrabStopped(object sender, GrabStopEventArgs e)
+        {
+            GrabOver = false;
+        }
+
+        private void StreamGrabber_ImageGrabbed(object sender, ImageGrabbedEventArgs e)
+        {
+            IGrabResult grabResult = e.GrabResult;
+            if (grabResult.IsValid)
+            {
+                if (GrabOver)
+                    CameraImageEvent(GrabResult2Bmp(grabResult));
+            }
+        }
+
+        private void Camera_ConnectionLost(object sender, EventArgs e)
+        {
+            camera.StreamGrabber.Stop();
+            DestroyCamera();
+        }
+
+        private void StreamGrabber_GrabStarted(object sender, EventArgs e)
+        {
+            GrabOver = true;
+        }
+
+        public void OneShot()
+        {
+            if (camera != null)
+            {
+                camera.Parameters[PLCamera.AcquisitionMode].SetValue(PLCamera.AcquisitionMode.SingleFrame);
+                camera.StreamGrabber.Start(1, GrabStrategy.OneByOne, GrabLoop.ProvidedByStreamGrabber);
+            }
+        }
+
+        public void KeepShot()
+        {
+            if (camera != null)
+            {
+                camera.Parameters[PLCamera.AcquisitionMode].SetValue(PLCamera.AcquisitionMode.Continuous);
+                camera.StreamGrabber.Start(GrabStrategy.OneByOne, GrabLoop.ProvidedByStreamGrabber);
+            }
+        }
+
+        public void Stop()
+        {
+            if (camera != null)
+            {
+                camera.StreamGrabber.Stop();
+            }
+        }
+
+        Bitmap GrabResult2Bmp(IGrabResult grabResult)
+        {
+            Bitmap b = new Bitmap(grabResult.Width, grabResult.Height, PixelFormat.Format32bppRgb);
+            BitmapData bmpData = b.LockBits(new Rectangle(0, 0, b.Width, b.Height), ImageLockMode.ReadWrite, b.PixelFormat);
+            pxConvert.OutputPixelFormat = PixelType.BGRA8packed;
+            IntPtr bmpIntpr = bmpData.Scan0;
+            pxConvert.Convert(bmpIntpr, bmpData.Stride * b.Height, grabResult);
+            b.UnlockBits(bmpData);
+            return b;
+        }
+
+        public void DestroyCamera()
+        {
+            if (camera != null)
+            {
+                camera.Close();
+                camera.Dispose();
+                camera = null;
+            }
+        }
+
+        private void MainForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            DestroyCamera();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!Directory.Exists(@"C:\Users\DEMO1\source\repos\BaslerCam1\Capture"))
+                {
+                    Directory.CreateDirectory(@"C:\Users\DEMO1\source\repos\USBCamApp1\Capture");
+                    MessageBox.Show("NO FLODER FOUND!!");
+                }
+                else
+                {
+                    string path = @"C:\Users\DEMO1\source\repos\BaslerCam1\Capture";
+                    pictureBox1.Image.Save(path + @"\" + textBox1.Text + ".jpg", ImageFormat.Jpeg);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
